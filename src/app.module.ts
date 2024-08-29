0879ce2410ce034aea35bccd26c6247554c8a16a
karberlee <karber@foxmail.com>
2024-08-29 17:21:13 +0800

    remove useless import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
-import { mongoConfig } from './config/mongo.config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from '@/modules/user/user.module'
